578e5105e1c02d8523c0d375959b283bc95cd05c
conor-mcgowan <conor.mcgowan@live.com>
2021-02-26 12:30:02 -0600

    adds react-router-dom and sets top-level structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+import React from "react";
+import {Switch, Route, Browser Router as Router, Redirect, Navlink, NavLink} from "react-router-dom";
+import LoginPage from "./components/login/LoginPage";
+import Homepage from "./components/homepage/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p></p>
-        <a></a>
-      </header>
-    </div>
+    <Router>
+      <>
+      <nav className="menu">
+        <NavLink to="/login" className="link text-center" activeClassName="active-link">Login</NavLink>
+        <NavLink to="/home" className="link text-center" activeClassName="active-link">Homepage</NavLink>
+        <NavLink to="/studies" className="link text-center" activeClassName="active-link">Lichess Studies</NavLink>
+        <NavLink to="/otbgames" className="link text-center" activeClassName="active=link">OTB Games by FEN</NavLink>
+      </nav>
+      </>
+    </Router>
   );
 }
 
