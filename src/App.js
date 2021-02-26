e27ca46f97defb6631567d2c57e4d36d67e79628
conor-mcgowan <conor.mcgowan@live.com>
2021-02-26 14:02:38 -0600

    updates comp. names, solidifies top-level routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,71 @@
 import "./App.scss";
 import React from "react";
-import {Switch, Route, Browser Router as Router, Redirect, Navlink, NavLink} from "react-router-dom";
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+  NavLink,
+} from "react-router-dom";
 import LoginPage from "./components/login/LoginPage";
-import Homepage from "./components/homepage/Homepage";
+import HomePage from "./components/homepage/HomePage";
+import Studies from "./components/studies/Studies";
+import FavoritePuzzles from "./components/favorite-puzzles/FavoritePuzzles";
+import OTBGames from "./components/otb-games/OTBGames";
 
 function App() {
   return (
     <Router>
       <>
-      <nav className="menu">
-        <NavLink to="/login" className="link text-center" activeClassName="active-link">Login</NavLink>
-        <NavLink to="/home" className="link text-center" activeClassName="active-link">Homepage</NavLink>
-        <NavLink to="/studies" className="link text-center" activeClassName="active-link">Lichess Studies</NavLink>
-        <NavLink to="/otbgames" className="link text-center" activeClassName="active=link">OTB Games by FEN</NavLink>
-      </nav>
+        <nav className="menu">
+          <NavLink
+            to="/login"
+            className="link text-center"
+            activeClassName="active-link"
+          >
+            Login
+          </NavLink>
+          <NavLink
+            to="/home"
+            className="link text-center"
+            activeClassName="active-link"
+          >
+            Homepage
+          </NavLink>
+          <NavLink
+            to="/studies"
+            className="link text-center"
+            activeClassName="active-link"
+          >
+            Lichess Studies
+          </NavLink>
+          <NavLink
+            to="/favpuzzles"
+            className="link text-center"
+            activeClassName="active-link"
+          >
+            Favorite Puzzles
+          </NavLink>
+          <NavLink
+            to="/otbgames"
+            className="link text-center"
+            activeClassName="active=link"
+          >
+            OTB Games by FEN
+          </NavLink>
+        </nav>
+        <main className="main-container">
+          <Switch>
+            <Route path="/login" component={LoginPage} />
+            <Route path="/home" component={HomePage} />
+            <Route path="/studies" component={Studies} />
+            <Route path="/favpuzzles" component={FavoritePuzzles} />
+            <Route path="/otbgames" component={OTBGames} />
+            <Route path="*">
+              <Redirect to="/home" />
+            </Route>
+          </Switch>
+        </main>
       </>
     </Router>
   );
